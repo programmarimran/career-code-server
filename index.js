bec15f1f3030aecde771cd14cc1637e52afa52ff
programmarimran <programmarimran@gmail.com>
2025-06-01 12:32:53 +0600

    addeda  a dynamic api with application_count is complited created

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function run() {
       const email = req.query.email;
       const query = {};
       if (email) {
-       query.hr_email =email;
+        query.hr_email = email;
       }
       const cursor = jobsCollection.find(query);
       const result = await cursor.toArray();
@@ -46,12 +46,25 @@ async function run() {
     //   const result=await jobsCollection.find(query).toArray()
     //   res.send(result)
     // })
+      app.get("/jobs/applications",async(req,res)=>{
+      const email=req.query.email;
+      const query={hr_email:email}
+      const jobs=await jobsCollection.find(query).toArray()
+      //should use aggrigate to optimum data fatching
+      for(const job of jobs ){
+        const applicationsQuery={jobId:job._id.toString()}
+        const application_count=await applicationsCollection.countDocuments(applicationsQuery)
+        job.application_count=application_count
+      }
+      res.send(jobs)
+    })
     app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
       res.send(result);
     });
+   
     app.post("/jobs", async (req, res) => {
       const cursor = req.body;
       const result = await jobsCollection.insertOne(cursor);
@@ -80,16 +93,29 @@ async function run() {
       }
       res.send(result);
     });
-    app.get("/applications/job/:job_id",async(req,res)=>{
-      const job_id=req.params.job_id;
-      const query={jobId:job_id}
-      const result=await applicationsCollection.find(query).toArray()
-      res.send(result)
-    })
+   
     app.delete("/applications/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await applicationsCollection.deleteOne(query);
+      res.send(result);
+    });
+    //holder job creater
+    app.get("/applications/job/:job_id", async (req, res) => {
+      const job_id = req.params.job_id;
+      const query = { jobId: job_id };
+      const result = await applicationsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.patch("/applications/:id", async (req, res) => {
+      const applicaionId = req.params.id;
+      const filter = { _id: new ObjectId(applicaionId) };
+      const updatedDoc = {
+        $set: {
+          status: req.body.status,
+        },
+      };
+      const result = await applicationsCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
